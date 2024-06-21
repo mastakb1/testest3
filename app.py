e49d221934c81c6264f163f9b53e5f37611a7eb6
mastakb1 <99850986+mastakb1@users.noreply.github.com>
2024-06-21 15:13:50 +0700

    Update app.py

--- app.py
+++ app.py
@@ -65,33 +65,3 @@ line_fig = px.line(daily_sales, x='fulldates', y='LineTotal', title='Tren Penjua
 st.plotly_chart(line_fig)
 st.write("Grafik ini menunjukkan bagaimana penjualan berubah setiap harinya sepanjang tahun yang dipilih. Pola ini dapat membantu mengidentifikasi tren musiman atau hari-hari dengan penjualan tertinggi.")
 st.write("Kesimpulan: Dari tahun ke tahun, penjualan mengalami naik turun, di awal tahun trend penjualan nya adalah naik, namun cenderung tidak signifikan. namun di tahun 2002 mengalami penjualan menurun, namun setelah itu malah terjadi lonjakan penjualan di tahun 2003 dan tahun selanjutnya memberikan gambaran tren penjualan yang naik")
-
-# Top 10 Produk Terlaris
-st.header('Top 10 Produk Terlaris')
-top_products = sales_product_df.groupby('name').agg({'LineTotal': 'sum'}).nlargest(10, 'LineTotal').reset_index()
-for index, row in top_products.iterrows():
-    progress_percent = row['LineTotal'] / top_products['LineTotal'].max()
-    st.write(f"{row['name']} - Total Penjualan: {row['LineTotal']}")
-    st.progress(progress_percent)
-st.write("Bagian ini menampilkan produk-produk dengan total penjualan tertinggi sepanjang tahun yang dipilih. Ini membantu mengidentifikasi produk-produk yang paling populer di kalangan pelanggan.")
-
-# Plot scatter relasi antara Order Quantity dan Total Penjualan
-st.header('Relasi Antara Unit Price dan Sales Amount')
-scatter_fig = px.scatter(sales_product_df, x='UnitPrice', y='LineTotal', color='category', title='Relasi Antara Unit Price dan Sales Amount')
-st.plotly_chart(scatter_fig)
-st.write("Scatter plot ini menunjukkan hubungan antara harga unit produk dan total penjualannya. Setiap titik mewakili satu produk, dengan warna yang berbeda untuk setiap kategori produk.")
-st.write("Kesimpulan : Hubungan ini menunjukan bahwa meskipun harga barang tinggi, tidak ada pengaruh nya dengan penjualan. Karena mungkin barang utama yang dijual adalah sepeda maka, meskipun harganya tinggi tetap saja penjualan tetap tinggi karena telah menemukan customer dengan niche yang pas")
-
-# Plot pie kontribusi kategori dalam penjualan
-st.header('Kontribusi Kategori dalam Penjualan')
-pie_fig = px.pie(category_sales_df, values='LineTotal', names='category', title='Kontribusi Kategori dalam Penjualan')
-st.plotly_chart(pie_fig)
-st.write("Diagram pie ini menunjukkan kontribusi masing-masing kategori produk terhadap total penjualan. Ini membantu memahami seberapa besar setiap kategori produk menyumbang terhadap pendapatan keseluruhan.")
-st.write(f"Kesimpulan : dari tahun 2001 - 2002 barang yang dijual hanyalah sepedah. bisa ditunjukan dari diagram pie yang 100% adalah sepedah. Dan mulai masuk barang dari kategori baru di tahun berikut nya, tetap saja yang berkontribusi banyak adalah barang dari kategori sepeda")
-
-# Plot histogram distribusi jumlah penjualan
-st.header('Distribusi Jumlah Penjualan')
-hist_fig = px.histogram(sales_time_hist_df, title='Distribusi Jumlah Penjualan')
-st.plotly_chart(hist_fig)
-st.write("Histogram ini menunjukkan distribusi jumlah penjualan. Ini membantu memahami variasi dalam jumlah penjualan dan mengidentifikasi rentang jumlah penjualan yang paling umum.")
-st.write("Kesimpulan: di tahun 2001, malah cenderung banyak penjualan dengan value tinggi. tahun 2002 mulai tersebar, namun masih banyak penjualan dari barang yang memiliki value tinggi.Namun di tahun tahun berikut nya, malah banyak penjualan dari barang bervalue rendah, mungkin dikarenakan toko tersebut mulai memasukan barang dengan harga murah dan barang dari kategori lain yang dianggap murah juga sebagai respon turun nya trend penjualan di tahun 2002")
